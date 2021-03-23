8d688225c9a5a497229d6bd32e2a426b625bcda3
PeSantanna <pedro.santana@ufu.br>
2021-03-23 16:14:46 -0300

    Faculdade

---
+++ Prolog/Roteiro 3/ex1.pl
@@ -0,0 +1,26 @@
+%?- member(a,[1,2,a,b]).
+
+%true
+
+%Ocorre pois 'a' é membro da lista indicada.
+
+
+%?- member(z,[1,2,a,b]).
+
+%false
+
+%Ocorre pois 'z' NAO é membro da lista indicada.
+
+
+%?- member(a,[1,2,a,b]).
+
+/*
+X = 1
+X = 2
+X = a
+X = b
+*/
+
+%Filtra todos os valores possiveis para a variavel dentro da lista.
+
+
