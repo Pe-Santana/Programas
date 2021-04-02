74c21936696c9b8f64254898cdf56e373c020d35
PeSantanna <pedro.santana@ufu.br>
2021-04-01 22:25:33 -0300

    Faculdade

--- Prolog/Roteiro 4/ex8.pl
+++ Prolog/Roteiro 4/ex8.pl
@@ -1,3 +1,39 @@
+%a)Check
+
+disjunto([],_).
+
+disjunto([X|XS],Y):-
+    \+ member(X,Y),
+    disjunto(XS,Y).
+
+
+%b)Check
+
+uniao([],[],[]).
+
+
+uniao([H,H1|T],[G,G1|T1],[H,H1,G,G1|V]):-
+    
+    uniao(T,T1,V).
+
+uniao([H|T],[G|T1],[H,G|V]):-
+    
+    uniao(T,T1,V).
+
+%c)Check
+
+intersecao([],_,[]).
+
+intersecao([X|XS],Y,G):-
+    member(X,Y),
+    G = [X|G1],
+    intersecao(XS,Y,G1).
+
+intersecao([_|XS],Y,G):-
+    intersecao(XS,Y,G).
+
+
+
 %d)
 
 diferenca([],_,[]).
