2c7fd2927e0db53054f161cf76221f39818b4f9b
PeSantanna <pedro.santana@ufu.br>
2021-05-10 18:54:38 -0300

    Faculdade

--- TAD15/Tlist.h
+++ TAD15/Tlist.h
@@ -1,7 +1,7 @@
 #define SUCCESS 0
 #define INVALID_NULL_POINTER -1
-#define SUCCESS -2
-#define OUT_OF_MEMORY -3
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
 #define ELEM_NOT_FOUND -4
 
 
@@ -13,7 +13,6 @@ typedef struct aluno
     float n1, n2, n3;
 }aluno;
 
-typedef struct list_node list_node;
 
 typedef struct TLinkedList TLinkedList;
 
@@ -23,5 +22,24 @@ TLinkedList *list_create();
 
 int list_free(TLinkedList *l);
 
+int list_push_front(TLinkedList *l, aluno a);
 
+int list_push_back(TLinkedList *l, aluno a);
 
+int list_insert(TLinkedList *l, int pos, aluno a);
+
+int list_size(TLinkedList *l);
+
+int list_print(TLinkedList *l);
+
+int list_pop_front(TLinkedList *l);
+
+int list_pop_back(TLinkedList *l);
+
+int list_erase_data(TLinkedList *l, int mat);
+
+int list_erase_pos(TLinkedList *l, int pos);
+
+int list_find_pos(TLinkedList *l, int pos, aluno *a); 
+
+int list_find_mat(TLinkedList *l, int mat, aluno *a);
