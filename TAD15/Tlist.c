a51c6555b7f5c15ee98c6ee170d5caa0d48573ec
PeSantanna <pedro.santana@ufu.br>
2021-05-10 18:53:53 -0300

    Faculdade

--- TAD15/Tlist.c
+++ TAD15/Tlist.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include "Tlist.h"
 
-typedef struct list_node
+typedef struct list_node list_node;
+struct list_node
 {
     aluno data;
     list_node *next;
-} list_node;
+};
 
 typedef struct TLinkedList
 {
@@ -45,6 +46,7 @@ int list_free(TLinkedList *l)
         }
         free(l);
     }
+    return SUCCESS;
 }
 
 int list_push_front(TLinkedList *l, aluno a)
@@ -57,6 +59,16 @@ int list_push_front(TLinkedList *l, aluno a)
     {
         list_node *node;
         node = malloc(sizeof(list_node));
+
+        if (node == NULL)
+        {
+            return OUT_OF_MEMORY;
+        }
+        node->data = a;
+        node->next = l->head;
+        l->head = node;
+
+        return SUCCESS;
     }
 }
 
@@ -91,6 +103,302 @@ int list_push_back(TLinkedList *l, aluno a)
             }
             aux->next = node;
         }
+        return SUCCESS;
+    }
+}
+
+int list_insert(TLinkedList *l, int pos, aluno a)
+{
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    else
+    {
+        list_node *node, *auxP, *auxA;
+        node = malloc(sizeof(list_node));
+        if (node == NULL)
+        {
+            return OUT_OF_MEMORY;
+        }
+        node->data = a;
+
+        if (pos == 1)
+        {
+            node->next = l->head;
+            l->head = node;
+
+            return SUCCESS;
+        }
+        else
+        {
+            auxP = l->head;
+            auxA = l->head;
+            for (int i = 1; i < pos; i++)
+            {
+                auxA = auxP;
+                auxP = auxP->next;
+                if (auxP == NULL && i < pos - 1)
+                {
+                    return OUT_OF_RANGE;
+                }
+            }
+            auxA->next = node;
+            node->next = auxP;
+
+            return SUCCESS;
+        }
+    }
+}
+int list_size(TLinkedList *l)
+{
+
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    else
+    {
+
+        int tamanho = 0;
+        list_node *aux;
+
+        aux = l->head;
+
+        while (aux != NULL)
+        {
+            aux = aux->next;
+            tamanho++;
+        }
+        return tamanho;
+    }
+}
+
+int list_print(TLinkedList *l)
+{
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    else
+    {
+        list_node *aux;
+
+        aux = l->head;
+
+        printf("\nImprimindo a lista (tamanho %i)\n", list_size(l));
+
+        while (aux != NULL)
+        {
+            printf("\n------------------\n");
+
+            printf("Matricula: %i\n", aux->data.matricula);
+            printf("Nome: %s\n", aux->data.nome);
+            printf("Notas: %.1f; %.1f; %.1f;\n", aux->data.n1, aux->data.n2, aux->data.n3);
+
+            aux = aux->next;
+        }
+        return SUCCESS;
+    }
+}
+
+int list_pop_front(TLinkedList *l)
+{
+
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    else
+    {
+        list_node *aux;
+
+        aux = l->head;
+        l->head = aux->next;
+
+        free(aux);
+
+        return SUCCESS;
+    }
+}
+
+int list_pop_back(TLinkedList *l)
+{
+
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    else
+    {
+        list_node *auxP, *auxA;
+
+        auxP = l->head;
+        auxA = l->head;
+
+        while (auxP->next != NULL)
+        {
+            auxA = auxP;
+            auxP = auxP->next;
+        }
+        auxA->next = auxP->next;
+
+        free(auxP);
+
+        return SUCCESS;
+    }
+}
+
+int list_erase_data(TLinkedList *l, int mat)
+{
+
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+
+    list_node *auxP, *auxA;
+
+    auxP = l->head;
+    auxA = l->head;
+
+    if (l->head->data.matricula == mat)
+    {
+        list_pop_front(l);
+        return SUCCESS;
+    }
+    while (auxP->next != NULL && mat != auxP->data.matricula)
+    {
+        auxA = auxP;
+        auxP = auxP->next;
+    }
+
+    if (auxP->next == NULL && mat != auxP->data.matricula)
+    {
+        return OUT_OF_RANGE;
+    }
+    if (mat == auxP->data.matricula)
+    {
+        auxA->next = auxP->next;
+
+        free(auxP);
+
+        return SUCCESS;
+    }
+    if (auxP->next == NULL && mat == auxP->data.matricula)
+    {
+        auxA->next = auxP->next;
+
+        free(auxP);
+
+        return SUCCESS;
+    }
+}
+
+int list_erase_pos(TLinkedList *l, int pos)
+{
+
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+
+    list_node *auxP, *auxA;
+
+    auxP = l->head;
+    auxA = l->head;
+
+    if (pos == 1)
+    {
+        list_pop_front(l);
+        return SUCCESS;
+    }
+
+    for (int i = 1; i < pos; i++)
+    {
+        auxA = auxP;
+        auxP = auxP->next;
+        if (auxP == NULL && i < pos - 1)
+        {
+            return OUT_OF_RANGE;
+        }
+    }
+    auxA->next = auxP->next;
+    free(auxP);
+
+    return SUCCESS;
+}
+
+int list_find_pos(TLinkedList *l, int pos, aluno *a)
+{
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    list_node *auxP, *auxA;
+
+    auxP = l->head;
+    auxA = l->head;
+
+    if (pos == 1)
+    {
+        *a = l->head->data;
+        return SUCCESS;
+    }
+
+    for (int i = 1; i < pos; i++)
+    {
+        auxA = auxP;
+        auxP = auxP->next;
+        if (auxP == NULL && i < pos - 1)
+        {
+            return OUT_OF_RANGE;
+        }
+    }
+    *a = auxP->data;
+    return SUCCESS;
+}
+
+int list_find_mat(TLinkedList *l, int mat, aluno *a)
+{
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    list_node *auxP, *auxA;
+
+    auxP = l->head;
+    auxA = l->head;
+
+    if (l->head->data.matricula == mat)
+    {
+        *a = l->head->data;
+        return SUCCESS;
+    }
+    while (auxP->next != NULL && mat != auxP->data.matricula)
+    {
+        auxA = auxP;
+        auxP = auxP->next;
+    }
+
+    if (auxP->next == NULL && mat != auxP->data.matricula)
+    {
+        return ELEM_NOT_FOUND;
+    }
+    if (mat == auxP->data.matricula)
+    {
+        auxA->next = auxP->next;
+
+        free(auxP);
+
+        return SUCCESS;
+    }
+    if (auxP->next == NULL && mat == auxP->data.matricula)
+    {
+        auxA->next = auxP->next;
+
+        free(auxP);
+
         return SUCCESS;
     }
 }
