8846a5d0b1884ad1537fc235946eddf3ecd49d76
PeSantanna <pedro.santana@ufu.br>
2021-05-31 19:37:11 -0300

    Prolog da massa

---
+++ Prolog/Trabalho Prolog/tbl_cliente.pl
@@ -0,0 +1,2 @@
+created(1622500310.667576).
+assert(clientes(11,'Mercearia do Cleber','Servidor','Atype','Juridica','865423','1111111','222222','Rua das Orquideas','Santo Gabriel','Uberaba','36754511','MG','865743412','Clebinho@gastronomia.br','Cleber','6666777889','Dono')).
---
+++ Prolog/Trabalho Prolog/tbl_usuarios.pl
@@ -0,0 +1,3 @@
+created(1622497376.110001).
+
+assert(usuarios(13,'Cleb','Cleber','1135','1135')).
