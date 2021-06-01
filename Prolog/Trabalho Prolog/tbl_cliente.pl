58708a9c0d1ee6f626b4b31e04c4a0e015d5fb93
PeSantanna <pedro.santana@ufu.br>
2021-06-01 13:01:09 -0300

    Alterado

--- Prolog/Trabalho Prolog/tbl_cliente.pl
+++ Prolog/Trabalho Prolog/tbl_cliente.pl
@@ -1,2 +1,3 @@
 created(1622500310.667576).
 assert(clientes(11,'Mercearia do Cleber','Servidor','Atype','Juridica','865423','1111111','222222','Rua das Orquideas','Santo Gabriel','Uberaba','36754511','MG','865743412','Clebinho@gastronomia.br','Cleber','6666777889','Dono')).
+assert(clientes(111,'Mercearia do maicon','Servidor2','Atype1','Juridica','8755636','2222222','333333','Rua das Amoras','Arvoredo','Uberlandia','34555621','MG','4331156','Maico@mercearia.br','Maion','345626245','Dono')).
--- Prolog/Trabalho Prolog/tbl_usuarios.pl
+++ Prolog/Trabalho Prolog/tbl_usuarios.pl
@@ -1,3 +1,4 @@
 created(1622497376.110001).
 
 assert(usuarios(13,'Cleb','Cleber','1135','1135')).
+assert(usuarios(44,'Maic','Maicon','2222','2222')).
