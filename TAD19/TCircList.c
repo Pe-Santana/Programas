97a996acbc7e1eb7d36953236cc58868d2f8fb4b
PeSantanna <pedro.santana@ufu.br>
2021-06-10 18:30:16 -0300

    Alterações programa

--- TAD19/TCircList.c
+++ TAD19/TCircList.c
@@ -9,15 +9,16 @@ struct CLNode
     CLNode *next;
 };
 
-typedef struct TCircList 
+typedef struct TCircList
 {
     CLNode *end;
 } TCircList;
 
-TCircList *list_create() {
+TCircList *list_create()
+{
 
     TCircList *list;
-    
+
     list = malloc(sizeof(TCircList));
 
     if (list != NULL)
@@ -26,10 +27,10 @@ TCircList *list_create() {
     }
 
     return list;
-
 }
 
-int list_free(TCircList *l){
+int list_free(TCircList *l)
+{
 
     if (l == NULL)
     {
@@ -72,7 +73,7 @@ int list_push_front(TCircList *l, aluno a)
         {
             return OUT_OF_MEMORY;
         }
-        
+
         node->data = a;
 
         if (l->end == NULL)
@@ -125,9 +126,9 @@ int list_print_forward(TCircList *l)
 {
     if (l == NULL)
         return INVALID_NULL_POINTER;
-    
+
     CLNode *aux;
-    aux = l->end;
+    aux = l->end->next;
     printf("\nImprimindo a lista\n");
 
     while (aux != l->end)
@@ -139,10 +140,73 @@ int list_print_forward(TCircList *l)
 
         aux = aux->next;
     }
+    printf("\n------------------\n");
+    printf("Matricula: %d\n", aux->data.matricula);
+    printf("Nome: %s\n", aux->data.nome);
+    printf("Notas: %.1f; %.1f; %.1f;\n", aux->data.n1, aux->data.n2, aux->data.n3);
+
     printf("\nFim da lista \n");
 }
 
+int list_insert(TCircList *l, int pos, aluno a)
+{
 
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    else
+    {
+        CLNode *node, *aux, *aux2;
 
+        node = malloc(sizeof(CLNode));
+        if (node == NULL)
+        {
+            return OUT_OF_MEMORY;
+        }
+        if (pos == 0)
+        {
+            list_push_front(l, a);
+        }
 
+        aux = l->end->next;
+        aux2 = l->end;
 
+        for (int i = 0; i < pos; i++)
+        {
+            aux = aux->next;
+            aux2 = aux2->next;
+        }
+        if (aux2 == l->end)
+        {
+            list_push_back(l, a);
+        }
+        else
+        {
+            node->data = a;
+            aux2->next = node;
+            node->next = aux;
+            return SUCCESS;
+        }
+    }
+}
+int list_size(TCircList *l)
+{
+
+    if (l == NULL)
+    {
+        return INVALID_NULL_POINTER;
+    }
+    else
+    {
+        int tamanho=1;
+        CLNode *aux;
+        aux = l->end->next;
+        while (aux != l->end)
+        {
+            aux = aux->next;
+            tamanho++;
+        }
+        return tamanho;
+    }
+}
--- TAD19/prog.c
+++ TAD19/prog.c
@@ -21,7 +21,18 @@ int main()
         {1170, "Vanessa da Cidade", 10, 6, 9},
         {1680, "Silvio Santos Junior", 7, 9, 9}};
 
-    
+    list_push_front(faculdade,A[2]);
+    list_push_back(faculdade,A[3]);
+    list_push_front(faculdade,A[1]);
+    list_push_front(faculdade,A[0]);
+    list_insert(faculdade,4,B[0]);
+
+    list_print_forward(faculdade);
+
+    int tamanho;
+    tamanho = list_size(faculdade);
+
+    printf("O tamanho da lista circular eh: %i",tamanho);
 
     list_free(faculdade);
 
